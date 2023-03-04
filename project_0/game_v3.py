107a3cb216f17e821b25164182572bfdef44dc1f
DmitriyGoryachev <gdn89@mail.ru>
2023-03-04 12:32:41 +0300

    save

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def random_predict(number:int=1) -> int:
+
+def random_predict(number: int = 1) -> int:
     """Randomly guess a number
 
     Args:
@@ -13,7 +14,7 @@ def random_predict(number:int=1) -> int:
     first = 0
     last = 101
     predict = 1
-    
+
     while number != predict:
         predict = np.random.randint(first, last)
         count += 1
@@ -21,9 +22,9 @@ def random_predict(number:int=1) -> int:
             first = predict
         elif number < predict:
             last = predict
-            
-            
-    return(count) 
+
+    return (count)
+
 
 def score_game(random_predict) -> int:
     """For how many attempts on average out of 1000 apporoaches your algorithm guesses
@@ -37,17 +38,15 @@ def score_game(random_predict) -> int:
     count_ls = []
     np.random.seed(1)
     random_array = np.random.randint(1, 101, size=(10000))
-    
+
     for number in random_array:
         count_ls.append(random_predict(number))
     score = int(np.mean(count_ls))
-    
+
     print(f"Your algorithm guesses number on average on {score} tries")
-    
-    
-    return(score)
+
+    return (score)
+
 
 if __name__ == '__main__':
     score_game(random_predict)
-
-
